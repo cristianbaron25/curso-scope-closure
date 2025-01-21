2258671362954b518866433ee3c6d390b324344e
Cristian Baron <cristianalejandrobaron@gmail.com>
2025-01-20 20:01:46 -0500

    declarar y redeclarar variables

---
+++ src/scope/re.js
@@ -0,0 +1,24 @@
+var firstName; // Undefined
+firstName = 'Cristian';
+console.log(firstName);
+
+var lastName = 'Baron'; //Declarar / asignar
+lastName = 'Mora';  //reasignar
+console.log(lastName);
+
+var secondName = 'Alejo'; //declarar / asignar
+var secondName = 'Alejandro'; //reasignando
+console.log(secondName);
+
+
+//Let
+let fruit = 'Apple'; //declarar y asignar
+fruit = 'Kiwi'; //reasignar
+
+let fruit = 'Banana';
+
+//const
+
+const animal = 'dog';
+animal = 'cat';
+console.log(animal);
